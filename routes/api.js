ae5c51f32e03e347fb7bb092e8c2aead2bef175d
James Xu <xujames007@gmail.com>
2019-03-10 17:45:38 -0400

    more security

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
 const fs = require('fs')
+const CryptoJS = require('crypto-js')
 const questions = JSON.parse(fs.readFileSync('./questions.json', 'utf-8'))
 var base64 = require('base-64');
+require('dotenv').load()
 
 router.post('/:q/:answer', function (req, res, next) {
     let answer = base64.decode(req.params.answer);
     if (questions[req.params.q]['answers'].includes(answer)) {
-        res.send("/q" + (Number(req.params.q.split("q")[1]) + 1))
+        res.send(base64.encode(CryptoJS.AES.encrypt("/q" + (Number(req.params.q.split("q")[1]) + 1), process.env.SECRET)))
     } else {
         res.send("wrong")
     }
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const CryptoJS = require('crypto-js')
+var base64 = require('base-64');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render("q1")
 });
 
 router.get('/:q', function(req, res, next) {
-  res.render(req.params.q)
+  console.log(CryptoJS.AES.decrypt(base64.decode(req.params.q), process.env.SECRET).toString(CryptoJS.enc.Utf8))
+  res.render(CryptoJS.AES.decrypt(base64.decode(req.params.q), process.env.SECRET).toString(CryptoJS.enc.Utf8).split("/")[1])
 })
 
 module.exports = router;
