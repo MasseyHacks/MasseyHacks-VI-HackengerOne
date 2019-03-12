ace9ffeeb767797a56c16d4a87577e27e8b086be
James Xu <xujames007@gmail.com>
2019-03-11 22:24:27 -0400

    encrypted answer endpoints

--- routes/api.js
+++ routes/api.js
@@ -7,12 +7,19 @@ const base64 = require('base-64')
 require('dotenv').load()
 
 router.post('/:q/:answer', function (req, res, next) {
-    let answer = base64.decode(req.params.answer);
-    if (questions[req.params.q]['answers'].includes(answer.toLowerCase())) {
-        res.send(base64.encode(CryptoJS.AES.encrypt("/q" + (Number(req.params.q.split("q")[1]) + 1), process.env.SECRET)))
+    let answer = base64.decode(req.params.answer)
+    if (req.params.q !== 'q1') {
+        console.log(req.params.q)
+        var q = CryptoJS.AES.decrypt(base64.decode(req.params.q), process.env.SECRET).toString(CryptoJS.enc.Utf8).split('/')[1]
+        console.log(q)
+    } else {
+        var q = req.params.q
+    }
+    if (questions[q]['answers'].includes(answer.toLowerCase())) {
+        res.send(base64.encode(CryptoJS.AES.encrypt("/q" + (Number(q.split("q")[1]) + 1), process.env.SECRET)))
     } else {
         res.send("wrong")
     }
 })
 
-module.exports = router;
+module.exports = router
